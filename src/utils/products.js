851d562e32bc5c77adab0fc4e4d4b31d2cdae00f
DarioUh <129867150+DarioUh@users.noreply.github.com>
2023-09-15 20:26:24 -0300

    Add files via upload

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,135 +1,132 @@
 const productos = [
-    //Pastas
+  
     {
         id: 1,
-        title: "Placa De Video ASUS Rog Strix 3070ti",
-        description: "Los bloques de construcción para la GPU más rápida y eficiente del mundo, el nuevo Ampere SM ofrece el doble de rendimiento FP32 y una eficiencia energética mejorada. Experimenta el doble de rendimiento que los RT Cores de primera generación, además de RT y sombreado simultáneos para un nivel completamente nuevo de rendimiento de trazado de rayos.",
-        stock: 15,
-        type: "Pastas",
+        title: "Yogur con frutas",
+        description: "Yogur clasico con frutas",
+        stock: 5,
+        type: "Lácteos",
         imageProduct: {
             firtsImage: "https://media.f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/7/7/7794820001301_1.png",
             secondImage: "https://media.ldlc.com/r1600/ld/products/00/05/74/25/LD0005742587_1_0005869333.jpg"
         },
-        price: 201000,
+        price: 600,
     },
 
     {
         id: 2,
-        title: "Placa De Video MSI rtx 3050",
-        description: "La última versión de la icónica serie GAMING de MSI ofrece una vez más el rendimiento, la eficiencia con bajo nivel de ruido y la estética que los jugadores incondicionales reconocen y confían. Ahora usted también puede disfrutar de todos sus juegos favoritos con una potente tarjeta gráfica que se mantiene fría y silenciosa. Justo como te gusta.",
+        title: "Caramelos",
+        description: "Arcor Caramelos Menta Chocolate 715g",
         stock: 7,
         type: "Golosinas",
         imageProduct: {
-            firtsImage: "https://www.fullh4rd.com.ar/img/productos/Pics_Prod/video-geforce-rtx-3050-8gb-msi-gaming-x-0.jpg",
+            firtsImage: "https://www.rimoldimayorista.com.ar/datos/uploads/mod_catalogo/31308/arcor-menta-chocolate-607b0d633848f_thumbnail.png?t=1618677091",
             secondImage: "https://www.fullh4rd.com.ar/img/productos/Pics_Prod/video-geforce-rtx-3050-8gb-msi-gaming-x-2.jpg"
         },
-        price: 99000,
+        price: 2190,
     },
 
     {
         id: 3,
-        title: "Placa De Video Nvidia Asus TUF RTX 3080",
-        description: "Nvidia es el fabricante líder de placas de video; su calidad asegura una experiencia positiva en el desarrollo del motor gráfico de tu computadora. Además, sus procesadores usan tecnología de punta para que puedas disfrutar de un producto veloz y duradero",
+        title: "Tapas la Salteña",
+        description: "Tapas para empanadas de hojaldre",
         stock: 20,
-        type: "Fiambres",
+        type: "Pastas",
         imageProduct: {
-            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_781239-MLA48656710911_122021-O.webp",
+            firtsImage: "https://www.molinos.com.ar/wp-content/uploads/2022/09/La-saltena-tapas-para-empanadas-hojaldradas-1.webp",
             secondImage: "https://http2.mlstatic.com/D_NQ_NP_859399-MLA49924537069_052022-O.webp"
         },
-        price: 300000,
+        price: 600,
     },
 
-    //procesadores
     {
         id: 4,
-        title: "Procesador AMD Ryzen™ 7 5700G",
-        description: "Bienvenido a la nueva era del rendimiento. La serie AMD Ryzen 7000 saca a relucir la velocidad descomunal de “Zen 4” y su potencia pura para que los jugadores y creadores puedan procesar cualquier juego o flujo de trabajo que se cruce en su camino. Los procesadores de PC más avanzados del mundo1 para jugadores y creadores amplían el liderazgo de rendimiento de AMD para potenciar tu PC.",
+        title: "Cremoso Paulina",
+        description: "Queso cremoso la paulina",
         stock: 6,
-        type: "Conservas",
+        type: "Fiambres",
         imageProduct: {
-            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_940934-MLU47593217192_092021-O.webp",
+            firtsImage: "https://superlago.com.ar/wp-content/uploads/2020/12/48004.jpg",
             secondImage: "https://http2.mlstatic.com/D_NQ_NP_642030-MLU47593199220_092021-O.webp"
         },
-        price: 65000,      
+        price: 6500,      
     },
 
     {
         id: 5,
-        title: "Procesador Intel Core i7-10700",
-        description: "Productividad y entretenimiento, todo disponible en tu computadora de escritorio. La superioridad tecnológica de INTEL es un beneficio para todo tipo de profesionales. Asegura el mejor rendimiento de las aplicaciones, de la transferencia de datos y la conexión con otros elementos tecnológicos. En este producto, encontrarás los núcleos, que son los encargados de ejecutar las instrucciones y actividades que le asignás a tu dispositivo.",
+        title: "Aceitunas rellenas",
+        description: "Aceitunas rellenas con morrón",
         stock: 6,
-        type: "Galletas",
+        type: "Conservas",
         imageProduct: {
-            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_729444-MLA42903822558_072020-O.webp",
+            firtsImage: "https://jmdistribuidora.com/wp/wp-content/uploads/2021/08/AC.-RELLENA-CON-MORRON-COD-J261-500x500.png",
             secondImage: "https://http2.mlstatic.com/D_NQ_NP_970575-MLA42767646612_072020-O.webp"
         },
-        price: 70000,      
+        price: 1700,      
     },
 
-    //mother
     {
         id: 6,
-        title: "Motherboard Asus Rog Strix B550-f",
-        description: "Las tarjetas madre de la serie ROG Strix B550 Gaming ofrecen buena parte de las características que puedes encontrar en la serie de gama alta ROG Strix X570 Gaming, como por ejemplo PCIe® 4.0. Con un suministro de energía potente y una refrigeración eficaz, la serie ROG Strix B550 Gaming está optimizada para las CPU AMD Ryzen™ de 3.ª Gen. Con una estética futurista y el intuitivo software ROG, la ROG Strix B550-F Gaming (Wi-Fi) es la plataforma ideal para montar el equipo de tus sueños.",
+        title: "Galletas Oreo",
+        description: "Galletas Oreo",
         stock: 3,
-        type: "Lácteos",
+        type: "Galletas",
         imageProduct: {
-            firtsImage: "https://mexx-img-2019.s3.amazonaws.com/Motherboard-Am4-Asus-Rog-Strix-B550-F-GAMING-WIFI-II_43255_1.jpeg",
+            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_745136-MLA50990680160_082022-O.webp",
             secondImage: "https://http2.mlstatic.com/D_NQ_NP_985217-MLA43783495710_102020-W.jpg"
         },
-        price: 54000,      
+        price: 540,      
     },
 
     {
         id: 7,
-        title: "Motherboard Gigabyte Ga B550 Aorus Elite",
-        description: "Placa base AMD B550 AORUS con diseño digital de energía doble de 12 + 2 fases, disipadores de calor de superficie agrandados, PCIe dual 4.0 / 3.0 x4 M.2 con protectores térmicos duales, Intel® WiFi 6802.11ax, LAN de 2.5GbE, USB frontal Type-C ™, RGB FUSION 2.0, Q-Flash Plus",
+        title: "Chupetin POPs",
+        description: "BSA. CHUPETIN MISTER POPS FRUTAL x50u",
         stock: 1,
-        type: "placa madre",
+        type: "Golosinas",
         imageProduct: {
-            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_836693-MLA45005140103_022021-O.webp"
+            firtsImage: "https://i0.wp.com/golmarymar.com.ar/wp-content/uploads/470.jpg?fit=1000%2C1000&ssl=1"
         },
-        price: 54000,      
+        price: 4100,      
     },
 
-    //fuentes
     {
         id: 8,
-        title: "Fuente de alimentación para PC Corsair",
-        description: "Con la fuente de alimentación Corsair RM750x podrás asegurar la corriente continua y estable de tu computadora de escritorio y optimizar el funcionamiento de sus componentes. A través de su sistema de refrigeración, podrás mantener la temperatura ideal de sus componentes y evitar su sobrecalentamiento. ",
+        title: "Salame La Paulina",
+        description: "Salame La Paulina",
         stock: 5,
-        type: "fuente",
+        type: "Fiambres",
         imageProduct: {
-            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_896876-MLA45978614530_052021-O.webp",
+            firtsImage: "https://superlago.com.ar/wp-content/uploads/2020/11/Salame-66.jpeg?v=76554ed971f4",
             secondImage: "https://http2.mlstatic.com/D_NQ_NP_789796-MLA45978850170_052021-O.webp"
         },
-        price: 31000,      
+        price: 3800,      
     },
 
     {
         id: 9,
-        title: "Fuente de alimentación para PC Giga-Byte",
-        description: "Con la fuente de alimentación Corsair RM750x podrás asegurar la corriente continua y estable de tu computadora de escritorio y optimizar el funcionamiento de sus componentes. A través de su sistema de refrigeración, podrás mantener la temperatura ideal de sus componentes y evitar su sobrecalentamiento.",
+        title: "Ravioles la Salteña",
+        description: "Pasta rellena con la mejor selección de queso mozzarella, danbo, parmesano y ricotta. Una solución deliciosa para todas tus comidas.",
         stock: 7,
-        type: "fuente",
+        type: "Pastas",
         imageProduct: {
-            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_839855-MLA45653444301_042021-O.webp",
+            firtsImage: "https://www.lasaltena.com.ar/wp-content/uploads/2020/03/raviolesquesospackfamiliar.jpg",
             secondImage: "https://http2.mlstatic.com/D_NQ_NP_980467-MLA45653376464_042021-O.webp"
         },
-        price: 19000,      
+        price: 1500,      
     },
 
     {
         id: 10,
-        title: "Fuente de alimentación para PC XPG",
-        description:"Con la fuente de alimentación XPG 850 podrás asegurar la corriente continua y estable de tu computadora de escritorio y optimizar el funcionamiento de sus componentes. Además, con su potencia de 850 W tendrás la energía suficiente para jugar y mirar tus series y videos favoritos por horas con un alto rendimiento",
+        title: "Leche Ilolay",
+        description:"Leche entera Ilolay",
         stock: 7,
-        type: "fuente",
+        type: "Lácteos",
         imageProduct: {
-            firtsImage: "https://http2.mlstatic.com/D_NQ_NP_891364-MLA45686629945_042021-O.webp",
+            firtsImage: "https://www.ilolay.com.ar/uploads/productos-slides/1634593717-base_productos-Quesos-untables-ralladoyleches.png",
             secondImage: "https://http2.mlstatic.com/D_NQ_NP_952421-MLA45686583993_042021-O.webp"
         },
-        price: 30000,      
+        price: 600,      
     },
 ]
 
